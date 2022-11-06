18aa21cdb4bd4372d88120c3289fe474d59a81c3
Ibeh Esther <estheribeh5@gmail.com>
2022-11-06 14:34:09 +0100

    refactor: Update getAllBlogs controller

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -22,11 +22,12 @@ const errorHandler = (error, req, res, next) => {
           })
           break;
         case "not found":
-          res.status(404).json({
-            error: "Not Found",
-            statusCode: 404,
-            message: "Resource not found"
-          })
+          // res.status(404).json({
+          //   error: "Not Found",
+          //   statusCode: 404,
+          //   message: "Resource not found"
+          // })
+          res.status(404).send("not found")
           break;  
         case "internal server error":
           res.status(500).json({
