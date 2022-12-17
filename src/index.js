6d98a56327c21f490d92c95d2669e8ff812aab6e
Ibeh Esther <estheribeh5@gmail.com>
2022-12-17 05:06:47 +0100

    config: enable rate limiting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const {limiter} = require("./config/config")
 const authRouter = require("./routes/auth");
 const { errorHandler } = require("./middlewares/error");
 const jwt_auth = require("./middlewares/auth");
@@ -27,6 +28,7 @@ mongoose.connection.on("error", (err) => {
 
 app.use(cors({origin: '*'}))
 app.use(express.json())
+app.use(limiter)
 
 // Connect express application to express routers
 app.use("/", authRouter);
